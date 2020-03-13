9746293390d7afda63f79f17ea1ca65eabebd159
sergiocasaspastor <scasap00@gmail.com>
2020-03-13 12:54:35 +0100

    add map for all the frames together

--- src/main.py
+++ src/main.py
@@ -18,6 +18,7 @@ from line_arguments import general_parser
 
 from config.utils import obtain_general_config
 from detectors.backgrounds.single_gaussian import obtain_global_var_mean
+from metrics.map_all_frames import calculate_ap
 
 SOURCE = "../datasets/AICity_data/train/S03/c010/vdo.avi"
 
@@ -68,10 +69,6 @@ def main(new_config):
         f = bgsg_module.get_contours
         for d in _DET_BACKGROUNDS:
             detectors[d] = f
-            
-    
-
-
     
 
     if gconf.gtruth.static:
@@ -87,6 +84,7 @@ def main(new_config):
                         save_plots=gconf.plots.iou.save)
     # mAP_plot = LinePlot("mAP_frame",max_val=350)
     detect_func = detectors[_detcr]
+    dt_rects_dict = {}
     
     while(cap.isOpened() and (not gconf.video.save_video and (gconf.STOP_AT == -1 or i <= gconf.STOP_AT) or
                               gconf.video.save_video and i <= VIDEO_END)):
@@ -101,7 +99,7 @@ def main(new_config):
             
             #Classify the result
             dt_rects = detect_func(frame)
-
+            dt_rects_dict[str(i)] = dt_rects
             #Obtain GT
             
             #Compute the metrics
@@ -152,7 +150,8 @@ def main(new_config):
                                                       (fshape[1],fshape[0]))
                     if(gconf.video.stack_iou):
                         f_out = iou_plot.last_img
-                    out_cap.write(f_out.astype('uint8'))
+                    if(gconf.video.save_video):
+                        out_cap.write(f_out.astype('uint8'))
                 cv2.imshow(w_name, f_out.astype('uint8'))
             
             # Press Q on keyboard to  exit
@@ -162,10 +161,13 @@ def main(new_config):
         # Break the loop
         else:
             break
+
+    print("mAP_allframes: {}".format(calculate_ap(dt_rects_dict, gt_frames, 535, 2140, 'random')))
     print("mIoU for all the video: ", np.mean(iou_history))
     print("mAP for all the video: ", np.mean(avg_precision))
     cap.release()
-    out_cap.release()
+    if(gconf.video.save_video):
+        out_cap.release()
     
     
 if __name__ == "__main__":
