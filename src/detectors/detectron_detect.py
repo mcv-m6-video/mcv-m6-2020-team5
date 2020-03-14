97f19a6d56110cf62a7f02bf5aeca4472e889824
Sergi Solà Casas <sergi@MacBook-MacBook-Pro-de-Sergi.local>
2020-03-15 00:32:11 +0100

    trainnn

--- src/detectors/detectron_detect.py
+++ src/detectors/detectron_detect.py
@@ -9,11 +9,15 @@ import pkg_resources
 
 import detectron2 
 from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
+from detectron2.engine import DefaultPredictor, DefaultTrainer
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
+from detectron2.data.catalog import DatasetCatalog
 from detectron2.data import MetadataCatalog
 import cv2
+from detectors.groundtruths.gt_modifications import obtain_gt
+
+import xmltodict
 
 MEAN_IMAGE = None
 
@@ -36,6 +40,7 @@ class detectron_detector(object):
                 self.cfg.merge_from_file(pkg_resources.resource_filename("detectron2.model_zoo", os.path.join("configs", retinanet_path)))
                 self.cfg.MODEL.WEIGHTS = model_zoo.ModelZooUrls.get(retinanet_path)
                 self.cfg.MODEL.RETINANET.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+                
             if network == "faster_rcnn":
                 self.cfg.merge_from_file(pkg_resources.resource_filename("detectron2.model_zoo", os.path.join("configs", faster_rcnn_path)))
                 self.cfg.MODEL.WEIGHTS = model_zoo.ModelZooUrls.get(faster_rcnn_path)
@@ -52,11 +57,38 @@ class detectron_detector(object):
 
             # Create predictor
         return DefaultPredictor(self.cfg)
+    
+    
+        
+    def generate_datasets(self, Ntraining):
+        
+        DatasetCatalog.register('training_set', get_dicts('train', Ntraining))
+        MetadataCatalog.get('training_set').set(thing_classes='Car')
         
     def train(self,training_frames):
         if(len(training_frames) <= 0):
             raise ValueError("The number of input frames must be bigger than 0")
-          
+        
+        self.generate_datasets(len(training_frames), method = 'random')
+        
+        #Crear el dataloader per al dataset d'entrenament.
+        self.cfg.DATASETS.TRAIN = ('training_set',)#Modificar
+        self.cfg.DATALOADER.NUM_WORKERS = 2
+        self.cfg.SOLVER.IMS_PER_BATCH = 2
+        self.cfg.SOLVER.BASE_LR = 0.001
+        self.cfg.SOLVER.MAX_ITER = 15000
+        self.cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256 
+        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(kitti_mots_dataset.thing_classes)#Modificar
+
+        os.makedirs(self.cfg.OUTPUT_DIR, exist_ok=True)
+        
+        trainer = DefaultTrainer(self.cfg)
+        trainer.resume_or_load(resume=False)
+        trainer.train()    
+        self.cfg.MODEL.WEIGHTS = os.path.join(self.cfg.OUTPUT_DIR, 'model_final.pth')
+        #Fer el train a aquí.Tot i això cal crear els dicts com fem al M5  
+        
+        return self.cfg
         
         
     def predict(self,frame):
@@ -88,4 +120,74 @@ class detectron_detector(object):
 def obtain_global_var_mean():
     global MEAN_IMAGE
     return np.dstack([MEAN_IMAGE]*3)
+
+def get_dicts(lists = 'train', N, method = 'random'):
+    img_dir="../datasets/AICity_data/train/S03/c010" 
+    annot_dir="../datasets/"
+    
+    filename = os.path.join(img_dir, "vdo.avi")
+    annotname = os.path.join(img_dir, "ai_challenge_s03_c010-full_annotation.xml")
+
+    dataset_dicts = []
+    dataset_train = []
+    dataset_val = []
+    
+    thing_classes = ["Car"]
+    
+    if not os.path.exists(dirName):
+        vidcap = cv2.VideoCapture(filename)
+        success,image = vidcap.read()
+        count = 0
+    
+        dirName = "frames"
+        os.mkdir(dirName)
         
+        while success:
+            cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+            success,image = vidcap.read()
+            print('Read a new frame: ', success)
+            count += 1
+    
+    for idx, img_name in tqdm(enumerate(os.listdir("../frames/")),desc='Getting dicts'):
+        record = {}
+        filename = os.path.join("../frames", img_name)
+        height, width = Image.open(filename).size
+
+        record["file_name"] = filename
+        record["image_id"] = idx
+        record["height"] = height
+        record["width"] = width
+        
+        #Refer les annotations
+        with open(annotname, "rb") as fd:
+            gdict = xmltodict.parse(fd)
+
+        objs = []
+        
+        frame_list = obtain_gt(include_parked = True)
+                
+        for coord in frame_list:
+            
+            bbox = [float(xy) for xy in coord]
+            obj = {
+                "bbox": bbox,
+                "bbox_mode": BoxMode.XYXY_ABS,
+                "category_id": thing_classes[0],
+                "iscrowd": 0
+                }
+            objs.append(obj)
+
+        record["annotations"] = objs
+        dataset_dicts.append(record)
+
+    dataset_train = dataset_dicts
+    
+        #Separar entre training i test (comprovar que estigui bé que és un copy paste molt a saco)
+    if method == 'random':
+        val_samples = random.choices(np.arange(0,len(self),1),k=int(len(self)*split))
+        val_img = [str(img).zfill(6)+'.png' for img in val_samples]
+        dataset_train = [dic for dic in dataset_dicts if not dic['file_name'].split('/')[-1] in val_img]
+        dataset_val = [dic for dic in dataset_dicts if dic['file_name'].split('/')[-1] in val_img]
+        
+    return dataset_train, dataset_val
+    
