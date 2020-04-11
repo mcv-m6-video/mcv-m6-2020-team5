b7bb6da15fac8e8b59acc26060b7b0b1d9288d17
Daniel <daniaz_ctrl4enviro@bitbucket.org>
2020-04-11 17:08:43 +0200

    improved interface

--- src/MTMC/display.py
+++ src/MTMC/display.py
@@ -26,9 +26,12 @@ def display_heatmap(dist_mat):
     row_maxs = np.max(dist_mat.numpy())
     new_matrix = (dist_mat.numpy() / row_maxs)
     
-    new_matrix -= np.mean(new_matrix)
-    new_matrix[new_matrix<0] = 0
-
+    # means_row = np.array(new_matrix.mean(1))
+    mean_total = new_matrix.mean()
+    
+    new_matrix[new_matrix>mean_total] = mean_total
+    new_matrix -= new_matrix.min()
+    
     row_maxs = np.max(new_matrix)
     new_matrix = (new_matrix / row_maxs)
         
@@ -38,10 +41,12 @@ def display_heatmap(dist_mat):
     
     new_matrix = 255-new_matrix
     # gray = cv2.cvtColor(new_matrix, cv2.COLOR_RGB2GRAY)
-    out = cv2.applyColorMap(new_matrix, cv2.COLORMAP_RAINBOW)
+    out = cv2.applyColorMap(new_matrix, cv2.COLORMAP_OCEAN)
     # cv2.imshow("res", out); cv2.waitKey(1000)
     return out
 
+sel_x, sel_y = -1, -1
+displaying = False
 def print_grid(img, p1, p2, color=(0, 0, 255)):
     # path1 = os.path.normpath(pickle1) 
     # path2 = os.path.normpath(pickle2) 
@@ -61,14 +66,40 @@ def print_grid(img, p1, p2, color=(0, 0, 255)):
         #     print("out of bounds!")
         #     print(counter,"-",len(p2[k])-1)
 
-displaying = False
-sel_x, sel_y = -1, -1
+def print_axis(img):
+    icpy = img.copy()
+    global displaying, sel_x, sel_y
+    if(displaying and sel_x > -1 and sel_y > -1):
+        if(sel_y < img.shape[0] and sel_x < img.shape[1]):
+            icpy[sel_y, :,:] = (255, 0, 0)
+            icpy[:, sel_x,:] = (255, 0, 0)
+        
+    return icpy
+
 def show_pair_imgs(event,x,y,flags,param):
-    if event == cv.EVENT_LBUTTONDOWN:
+    global displaying, sel_x, sel_y
+    if event == cv2.EVENT_LBUTTONDOWN:
         displaying = True
-        ix,iy = x,y
-    elif event == cv.EVENT_MOUSEMOVE:
+        # ix,iy = x,y
+    elif event == cv2.EVENT_MOUSEMOVE:
         if displaying == True:
             pass
-    elif event == cv.EVENT_LBUTTONUP:
-        drawing = False
+    elif event == cv2.EVENT_LBUTTONUP:
+        displaying = False
+    
+    if displaying:
+        sel_x, sel_y = x, y
+        # c1 = param[0]
+        # c2 = param[1]
+        # # print("C1:", len(c1), "x:", x)
+        # # img1 = c1[y]
+        # # img2 = c2[x]
+        # # print("img1:", img1)
+        # cv2.imshow("car1", img1)
+        # cv2.imshow("car2", img2)
+        # cv2.waitKey(100)
+    # else:
+        # sel_x, sel_y = -1, -1
+def obtain_xy():
+    global sel_x, sel_y 
+    return sel_x, sel_y
--- src/MTMC/test.py
+++ src/MTMC/test.py
@@ -12,6 +12,8 @@ import pickle
 from model import Model
 from metrics import calculate_matrices
 from collections import OrderedDict
+import glob
+
 # classifies the dictionary by track instead of by frames
 def regenerate_tracks(camera_dict):
     track_dict = {}
@@ -49,13 +51,15 @@ def generate_track_for_all_cams(in_path, sequence_num, camera_list):
 
 def generate_features(all_cam_dict, in_path, sequence_num, camera_list,
                       save_path = "./out/cams", feature_func=None,
-                      return_images = False):
+                      return_images = False, max_permitted_size = 150*150*3):
     
     if not os.path.exists(save_path):
-        os.makedirs(save_paths)
+        os.makedirs(save_path)
     seq_name = "S{:02d}".format(sequence_num)
     in_path = os.path.join(in_path,seq_name)
     cam_pickles = {}
+    img_pickles = {}
+    print("CAM LIST:", camera_list)
     for cam in tqdm(camera_list):
         feature_accumulator = OrderedDict()
         if(return_images):
@@ -87,6 +91,9 @@ def generate_features(all_cam_dict, in_path, sequence_num, camera_list,
                             img_accumulator[track_id] = []
                     feature_accumulator[track_id].append(feature)
                     if(return_images):
+                        if(cropped_bbox.size > max_permitted_size):
+                            rf = np.sqrt(max_permitted_size/cropped_bbox.size)
+                            cropped_bbox = cv2.resize(cropped_bbox,None, fx=rf, fy=rf)
                         img_accumulator[track_id].append(cropped_bbox)
                     # feature_accumulator.append((track_id,feature))
             i += 1
@@ -103,10 +110,11 @@ def generate_features(all_cam_dict, in_path, sequence_num, camera_list,
             print(f"Saving images for cam {cam}")  
             ppath2 = os.path.join(save_path, f"{cam}_imgs.pkl")
             with open(ppath2, "wb+") as f:
-                pickle.dump(feature_accumulator, f)
-            
+                pickle.dump(img_accumulator, f)
+            del img_accumulator
             img_pickles[cam] = ppath2
-            return cam_pickles, img_pickles
+    if(return_images):
+        return cam_pickles, img_pickles
     return cam_pickles
 
 def inside_dims(idx, mn, mx):
@@ -137,8 +145,11 @@ if __name__ == "__main__":
     out_path = "./out/cams"
     sequence = 3
     cameras = [10, 11, 12, 13, 14, 15]
+    # cameras = [10, 11]
     fc_normalize = False
     load_pickles = True
+    show_cars = True
+    max_permitted_size = 150*150*3
     
     all_cam_dict = generate_track_for_all_cams(in_path,sequence,cameras)
     
@@ -148,14 +159,28 @@ if __name__ == "__main__":
     model = Model(dir_to_weights)
     # load_pretrained_weights(model, dir_to_weights)
     ppath = os.path.join(out_path, f"cam_pickles.pkl")
+    ipath = os.path.join(out_path, f"cam_imgs_pickles.pkl")
     if not load_pickles:
-        cam_pickles = generate_features(all_cam_dict,in_path,sequence,
+        _r = generate_features(all_cam_dict,in_path,sequence,
                                                 cameras, save_path=out_path,
-                                                feature_func=model)
+                                                feature_func=model,
+                                                return_images = show_cars,
+                                                max_permitted_size=max_permitted_size)
+        if(not show_cars):
+            cam_pickles = _r
+        else:
+            cam_pickles, img_pickles = _r
+            with open(ipath, "wb+") as f:
+                pickle.dump(img_pickles, f)
         with open(ppath, "wb+") as f:
             pickle.dump(cam_pickles, f)
     else:
-        cam_pickles = pickle.load(open(ppath, "rb"))
+        # if(not show_cars):
+        cam_pickles = {c: os.path.join(out_path, f"{c}.pkl") for c in cameras}
+        # cam_pickles = pickle.load(open(ppath, "rb"))
+        if(show_cars):
+            # img_pickles = pickle.load(open(ipath, "rb"))
+            img_pickles = {c: os.path.join(out_path, f"{c}_imgs.pkl") for c in cameras}
 
     
     relation_cams = calculate_matrices(cam_pickles)
@@ -165,23 +190,52 @@ if __name__ == "__main__":
     import display
     # display.display_heatmap(relation_cams[10][11])
     # a = "y"
+    cv2.namedWindow("res")
     i = 11
+    j = 10
+    print(cam_pickles)
+    p1 = pickle.load(open(cam_pickles[j], "rb"))
+    if(show_cars):
+        pc1 = pickle.load(open(img_pickles[j], "rb"))
+        c1 = []
+        for k in pc1.keys(): c1.extend(pc1[k]) 
+    
     while  i < 16:
-        p1 = pickle.load(open(cam_pickles[10], "rb"))
+        # print(cam_pickles)
         p2 = pickle.load(open(cam_pickles[i], "rb"))
-        dists = relation_cams[10][i]
+        if(show_cars):
+            pc2 = pickle.load(open(img_pickles[i], "rb"))
+            c2 = []
+            for k in pc2.keys(): c2.extend(pc2[k]) 
+        dists = relation_cams[j][i]
         trk_v_res = relate_tracks(dists, p1, p2)
     
-        res = display.display_min(dists)
+        res = display.display_heatmap(dists)
         display.print_grid(res, p1, p2)
         # a = input("Continue? y/n")
-        cv2.imshow("res", res)
         k = -1
         pressed = False
+        if(show_cars):
+            cv2.setMouseCallback("res", display.show_pair_imgs, (c1, c2))
         while not pressed:
-            print("press 'c' to continue")
-            k = cv2.waitKey(0)
-            print("key:", k)
+            # rshow = display.obtain_img(res, display.sel_x, display.sel_y)
+            cv2.imshow("res", res)
+            x,y = display.obtain_xy()
+            # print(x, y)
+            try:
+                img1 = c1[y]
+                img2 = c2[x]
+            except:
+                pass
+            if(x > -1 and y > -1):        
+                cv2.imshow("car1", img1)
+                cv2.imshow("car2", img2)
+                res_i = display.print_axis(res)
+                cv2.imshow("res", res_i)
+            k = cv2.waitKey(1)
+            if(k != -1):
+                print("press 'c' to continue")
+                print("key:", k)
             if(k == 99):
                 i+=1
                 pressed = True
